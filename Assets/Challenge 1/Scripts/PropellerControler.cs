4f8a8ea62db0835776e4251ad32ca3dcf46c8013
Juan Carlos Neíl Palacios Escobar <67353415+JuanK-2003@users.noreply.github.com>
2022-02-10 10:01:03 -0600

    Assets

---
+++ Assets/Challenge 1/Scripts/PropellerControler.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerControler : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
